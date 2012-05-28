07b7d2d2a9e00ab28d8ac37dbc99409ce0567be6
Richard Dallaway <richard@spiralarm.com>
2012-05-28 19:04:36 +0100

    Split the build into a "first" and "second" step: the "second" modules are those that depend on the "first".

--- project/LiftModulesBuildAll.scala
+++ project/LiftModulesBuildAll.scala
@@ -5,6 +5,8 @@ object LiftModulesBuildAll extends Build {
 
 	val liftVersion = SettingKey[String]("liftVersion", "The version of the Lift Web Framework to build against")
 	
+  val scalaVersions =  Seq("2.8.1", "2.9.0-1", "2.9.1", "2.9.2")
+
 	// Git "read-only" URLs appear to be the ones to use here:
 	lazy val modules: Seq[ProjectReference] = List(
 		uri("git://github.com/liftmodules/paypal.git"),
@@ -15,29 +17,40 @@ object LiftModulesBuildAll extends Build {
 		uri("git://github.com/liftmodules/jta.git"),
 		uri("git://github.com/liftmodules/machine.git"),
 		uri("git://github.com/liftmodules/oauth.git"),
-		// NB: oauth-mapper build is triggered by a build of oauth and this build, so no need to include it here
-		// uri("git://github.com/liftmodules/oauth-mapper.git"),
 		uri("git://github.com/liftmodules/openid.git"),
 		uri("git://github.com/liftmodules/scalate.git"),
 		uri("git://github.com/liftmodules/textile.git"),
 		uri("git://github.com/liftmodules/xmpp.git")
-		//uri("git://github.com/d6y/liftmodules-googleanalytics.git")
+		uri("git://github.com/d6y/liftmodules-googleanalytics.git")
 		//uri("git://github.com/d6y/liftmodules-imap-idle.git")
 		)
 	
-	// Override the version of Lift and publish settings for all modules being built:
-	lazy val moduleSettings: Seq[Setting[_]] = modules.flatMap { module => List( 
+	 // Modules that have a dependency on one or more of the modules above
+   lazy val dependent: Seq[ProjectReference] = List(
+			uri("git://github.com/liftmodules/oauth-mapper.git") // depends on oauth
+   	)
+
+	 // Override the version of Lift and publish settings for all modules being built:
+   def setting(module: ProjectReference) = List( 
 		liftVersion in module := "2.5-SNAPSHOT",
 		resolvers in module +=  "Central snapshot" at "https://oss.sonatype.org/content/repositories/snapshots/",
 		publishTo in module := Some("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"),
 		credentials in module += Credentials( file("/private/liftmodules/sonatype.credentials") )
 		)
-	}
 
-  lazy val all = Project(id = "all", base = file(".")).
+	lazy val moduleSettings: Seq[Setting[_]] = modules.flatMap(setting)
+
+  lazy val first = Project(id = "first", base = file(".")).
   	aggregate(modules:_*).
  		settings(publish := { }). // don't publish this wrapper project 
- 		settings(crossScalaVersions := Seq("2.8.1", "2.9.0-1", "2.9.1", "2.9.2")).
+ 		settings(crossScalaVersions := scalaVersions).
   	settings(moduleSettings:_*)
+
+  lazy val second = Project(id = "second", base = file(".")).
+  	aggregate(dependent:_*).
+ 		settings(publish := { }). 
+ 		settings(crossScalaVersions := scalaVersions).
+  	settings(dependent.flatMap(setting):_*)
+
 
 }
